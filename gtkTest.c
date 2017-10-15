d6cc319ec0db2ef0ec86a59f419463955ff41f86
Roman A. Paramonov <paramra@mail.ru>
2017-10-15 10:43:27 +0300

    Add redraw

--- gtkTest.c
+++ gtkTest.c
@@ -3,9 +3,14 @@
 GtkWidget	*btTest;
 GtkWidget	*drawTest;
 
+int flag = 0;
 void on_btTest_clicked(void)
 {
-	printf("Test\n");
+
+	flag = 1;
+	gtk_widget_queue_draw(drawTest);
+	printf("Redraw\n");
+
 }
 gboolean on_drawTest_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
@@ -20,10 +25,19 @@ gboolean on_drawTest_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 
 	gtk_render_background (context, cr, 0, 0, width, height);
 
+	//cairo_image_surface_create_from_p
+
+	if(flag)
+		cairo_rectangle(cr, 0.0, 0.0, 159.0, 127.0);
+
 	cairo_arc (cr,
 			width / 2.0, height / 2.0,
 			MIN (width, height) / 2.0,
 			0, 2 * G_PI);
+	//cairo_rectangle(cr, 0.0, 0.0, 159.0, 127.0);
+
+	cairo_move_to(cr, 0, 0);
+	cairo_line_to(cr, 159, 0);
 
 	gtk_style_context_get_color (context,
 			gtk_style_context_get_state (context),
